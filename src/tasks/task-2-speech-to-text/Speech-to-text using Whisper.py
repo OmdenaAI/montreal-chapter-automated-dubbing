03e14d252a85c0f8d65355cf7f967f931795dc3d
ADITI GULATI <83702433+aditi-gulati@users.noreply.github.com>
2023-06-05 15:39:00 +0530

    Speech-to-text using Whisper
    1. Detect the language. 
    2. Extract transcript, timestamps, and speaker features.
    3. Convert the transcript to the known language.
    4. Generate a list of tuples with phrase/word, timestamp, and speaker features.

---
+++ src/tasks/task-2-speech-to-text/Speech-to-text using Whisper.py
@@ -0,0 +1,61 @@
+import numpy as np
+from openai import Whisper
+from langdetect import detect
+
+# Initialize the Whisper ASR system
+whisper = Whisper()
+
+#  Function to detect the language of the given text
+def detect_language(text):
+    return detect(text)
+
+#  Function to perform speech-to-text conversion
+def speech_to_text(audio_file, known_language):
+    # Load audio file
+    audio, sample_rate = whisper.read_audio(audio_file)
+
+    # Perform speech recognition
+    results = whisper.transcribe(audio, sample_rate)
+
+    # Extract transcript, timestamps, and speaker features
+    transcript = results['transcripts'][0]['text']
+    timestamps = results['transcripts'][0]['timestamps']
+    speaker_features = extract_speaker_features(transcript)
+
+    # Convert transcript to the known language
+    if detect_language(transcript) != known_language:
+        transcript = translate_text(transcript, known_language)
+
+    # Generate list of tuples with phrase/word, timestamp, and speaker features
+    result = [(transcript, start_time, speaker_features) for _, start_time, _ in timestamps]
+
+    return result
+
+#  Function to extract speaker features (e.g., gender) based on the transcript
+def extract_speaker_features(transcript):
+    # You can implement your own logic here to extract speaker features from the transcript
+    # For example, you can use NLP techniques, keywords, or predefined dictionaries
+
+    # Placeholder logic
+    gender = "Unknown"
+    # ...
+
+    # Return speaker features
+    return gender
+
+# Function to translate text to the specified language
+def translate_text(text, target_language):
+    # You can use your preferred translation API or library here
+    # For example, you can use Google Translate API or the 'translate' library
+
+    # Placeholder translation logic
+    translated_text = text + " (translated to " + target_language + ")"
+    # ...
+
+    return translated_text
+
+# Example usage
+audio_file = "path/to/your/audio.wav"
+known_language = input("Enter your known language (e.g., 'en' for English, 'fr' for French, 'de' for German, 'es' for Spanish): ")
+output = speech_to_text(audio_file, known_language)
+print(output)
